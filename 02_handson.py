b31af59d15039f3181354d66ed7c3c2fea7fde88
Andreas Moor <github@grmnd.ch>
2020-11-13 18:11:38 +0100

    add execises

--- 02_handson.py
+++ 02_handson.py
@@ -1,6 +1,8 @@
 #%%
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 # %%
 housing = pd.read_csv('datasets/housing/housing.csv')
 # %%
@@ -17,7 +19,31 @@ for train_index, test_index in split.split(housing, housing["income_cat"]):
 for set_ in (strat_train_set, strat_test_set):
     set_.drop("income_cat", axis=1, inplace=True)
 # %%
-strat_train_set
+strat_train_set.head()
 # %%
 housing = strat_train_set.copy()
+housing.head()
+# %%
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+    s=housing["population"]/100, label="population", figsize=(10,7),
+    c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True,
+    sharex=False)
+plt.legend()
+# %%
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+# %%
+from pandas.plotting import scatter_matrix
+attributes = ["median_house_value", "median_income", "total_rooms",
+              "housing_median_age"]
+scatter_matrix(housing[attributes], figsize=(12, 8))
+# %%
+housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_household"]=housing["population"]/housing["households"]
+# %%
+housing.plot(kind="scatter", x="rooms_per_household", y="median_house_value",
+             alpha=0.2)
+plt.axis([0, 5, 0, 520000])
+plt.show()
 # %%
